81b33ef86175c993df2618d5ec925994c99ba8e4
Dixon, Evan <edixon@mimeo.com>
2026-08-13 15:03:46 -0500

    Let custom menu items be links

---
+++ Mimeo.DynamicUI.Blazor/Forms/CustomMenuLink.cs
@@ -0,0 +1,8 @@
+﻿namespace Mimeo.DynamicUI.Blazor.Forms
+{
+    public class CustomMenuLink : CustomMenuItemBase
+    {
+        public required string Path { get; set; }
+        public string? Target { get; set; }
+    }
+}
